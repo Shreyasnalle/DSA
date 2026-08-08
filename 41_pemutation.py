24ef3f62348480d00657bd6856df7e64b3d8ecc6
Shreyasnalle <shreyas.nalle7@gmail.com>
2026-08-08 23:57:51 +0530

    41_permutation 8-8-26

---
+++ 41_pemutation.py
@@ -0,0 +1,21 @@
+class Solution:
+    def permuteUnique(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        res = []
+        used = [False] * len(nums)
+        def backtrack(curr):
+            if len(curr) == len(nums):
+                res.append(curr[:])
+                return
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue
+                used[i] = True
+                curr.append(nums[i])
+                backtrack(curr)
+                curr.pop()
+                used[i] = False
+        backtrack([])
+        return res
