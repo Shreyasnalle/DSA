20598fc30c5ae152a67e454ebf541a0e575ace83
Shreyasnalle <shreyas.nalle7@gmail.com>
2026-08-18 21:07:14 +0530

    zero at end 18-8-26

---
+++ 283_zero_at_end.py
@@ -0,0 +1,7 @@
+class Solution:
+    def moveZeroes(self, nums: list[int]) -> None:
+        insert_position = 0
+        for i in range(len(nums)) :
+            if nums[i] != 0 :
+                nums[insert_position], nums[i] = nums[i], nums[insert_position]
+                insert_position += 1
