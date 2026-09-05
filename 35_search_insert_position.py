6ba9c94b7a613acbab8844705f6509b23ee1569c
Shreyasnalle <shreyas.nalle7@gmail.com>
2026-09-05 14:08:27 +0530

    search insert position 5-9-26

---
+++ 35_search_insert_position.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        answer = len(nums)
+        while low <= high :
+            mid = (low + high) // 2 
+            if nums[mid] >= target :
+                answer = mid 
+                high = mid - 1
+            else :
+                low = mid + 1
+        return answer
